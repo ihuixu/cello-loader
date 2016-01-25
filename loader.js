1434d0dd2dd067eaf789d0147a0cf9f5bfd86b2b
xh1021 <xh1021@vip.qq.com>
2016-01-25 15:21:25 +0800

    loader

--- loader.js
+++ loader.js
@@ -7,7 +7,7 @@ loader.loader = function(hostPath, debug){
 		opts.depends && opts.depends.map(function(v){
 			tags.push('<script src="' + hostPath + filePath + '/' + v +'.js' + '"></script>')	
 		})
-		tags.push('<script src="' + hostPath + filePath + (opts.depends ? '~'+opts.depends.join(',') : '') + '/' + mainPath +'.js' + '"></script>')	
+		tags.push('<script src="' + hostPath + filePath + ((opts.depends && opts.depends.length) ? '~'+opts.depends.join(',') : '') + '/' + mainPath +'.js' + '"></script>')	
 
 		return tags.join('')
 	} 
